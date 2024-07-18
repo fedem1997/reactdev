c31e4e766513f313e225764852b5992acad37819
Fede Meoniz <fedemeoniz97@hotmail.com>
2024-07-18 16:17:46 -0300

    Follow Card Twitter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App (){
     return(
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="Mi avatar" src="https://unavatar.io/kikobeats"/>
-                <div className='tw-followCard-info'>
-                    <strong>Miguel Angel</strong>
-                    <span className='tw-followCard-infoUserName'>@mmiguel</span>
-                </div>
-            </header>
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className="App">
+        <TwitterFollowCard  userName="fedem1997" name="Fede Meoniz"/>
+        <TwitterFollowCard  userName="midudev" name="Miguel Angel"/>
+        </section>
     )  
 }
